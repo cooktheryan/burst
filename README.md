7c42d49c73b2800cbf7b0a23405c44782739d0ad
Ryan Cook <rcook@redhat.com>
2016-10-20 12:28:53 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Quick PHP page to support the burst project in OpenShift
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>Burst Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Burst Project</p>'; ?> 
+ </body>
+</html>
